2f4d4f6ce61be48384ce90cd6365bd8a1f25d84c
Colin von Grimmenstein <Cvongrimmenstein@sharpencx.com>
2018-11-27 15:59:43 -0500

    Initial commit

---
+++ App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import App from './App';
+
+describe('App', () => {
+  describe('Structure', () => {
+    it('renders correctly', () => {
+      const wrapper = shallow(<App />);
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+});
